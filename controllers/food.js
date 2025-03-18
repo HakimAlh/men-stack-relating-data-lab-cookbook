865219bf0c052111c7d8fbec166ff8a3d5af25f4
Hakim <hakimalhaddad2@gmail.com>
2025-03-18 14:05:04 +0300

    Fixed Edit and Delete

--- controllers/food.js
+++ controllers/food.js
@@ -1,102 +1,114 @@
 // controllers/foods.js
 
-const express = require('express');
+const express = require("express");
 
-const User = require('../models/user.js');
-  
-  const newFood = (req, res) => {
-    res.render('foods/new.ejs', {
-        title: 'New Food'
-    })
-  };
+const User = require("../models/user.js");
 
-  const foodCreate = async (req, res) => {
-    try {  
-        const currentUser = await User.findById(req.params.userId)
-        currentUser.pantry.push(req.body) // pushing the formData into the user model
-        await currentUser.save() // save our edits
-        res.redirect(`/users/${currentUser._id}/foods`)
-    } catch (err) {
-        console.log(err)
-        res.redirect('/')
-    }
-  }
+const newFood = (req, res) => {
+	res.render("foods/new.ejs", {
+		title: "New Food",
+	});
+};
 
-    const index = async (req, res) => {
-      try {
-          const currentUser = await User.findById(req.params.userId)
-          res.render('foods/index.ejs', {
-              title: 'Food',
-              pantry: currentUser.pantry,
-          })
-      } catch (err) {
-          console.log(err)
-          res.redirect('/')
-      }
-}
+const foodCreate = async (req, res) => {
+	console.log("===============================================", req.body);
+	try {
+		const currentUser = await User.findById(req.params.userId);
+    console.log('currentUser: ', currentUser)
+		currentUser.pantry.push(req.body); // pushing the formData into the user model
+		await currentUser.save(); // save our edits
+		res.redirect(`/users/${currentUser._id}/foods`);
+	} catch (err) {
+		console.log(err);
+		res.redirect("/");
+	}
+};
 
-  const pantry = async (req, res) => {
-    res.render('./foods/pantry.ejs', {
-      title: 'Pantry'
-    })
-  }
+const index = async (req, res) => {
+	try {
+		const currentUser = await User.findById(req.params.userId);
+		res.render("foods/index.ejs", {
+			title: "Food",
+			pantry: currentUser.pantry,
+		});
+	} catch (err) {
+		console.log(err);
+		res.redirect("/");
+	}
+};
+
+const pantry = async (req, res) => {
+	res.render("./foods/pantry.ejs", {
+		title: "Pantry",
+	});
+};
 
 const show = async (req, res) => {
-  try {
-      const currentUser = await User.findById(req.params.userId)
-      const food = currentUser.pantry.id(req.params.foodId)
-      res.render('foods/show.ejs', {
-          title: food.title,
-          food,
-      })
-  } catch (err) {
-      console.log(err)
-      res.redirect('/')
-  }
-}
+	try {
+		const currentUser = await User.findById(req.params.userId);
+		const food = currentUser.pantry.id(req.params.foodId);
+		res.render("foods/show.ejs", {
+			title: food.title,
+			food,
+		});
+	} catch (err) {
+		console.log(err);
+		res.redirect("/");
+	}
+};
 
 const deleteFood = async (req, res) => {
-  try {
-      console.log('inside delete')
-      const currentUser = await User.findById(req.params.userId)
-      currentUser.pantry.id(req.params.foodId).deleteOne()
-      await currentUser.save()
-      res.redirect(`/users/${currentUser._id}/foods`)
-  } catch (err) {
-      console.log(err)
-      res.redirect('/')
-  }
-}
+	try {
+		console.log("inside delete");
+		const currentUser = await User.findById(req.params.userId);
+		currentUser.pantry.id(req.params.foodId).deleteOne();
+		await currentUser.save();
+		res.redirect(`/users/${currentUser._id}/foods`);
+	} catch (err) {
+		console.log(err);
+		res.redirect("/");
+	}
+};
 
 const edit = async (req, res) => {
-  try {
-      const currentUser = await User.findById(req.params.userId)
-      const food = currentUser.pantry.id(req.params.foodId)
-      res.render('foods/edit.ejs', {
-          title: food.title,
-          food,
-      })
-  } catch (err) {
-      console.log(err)
-      res.redirect('/')
-  }
-}
+	try {
+		const currentUser = await User.findById(req.params.userId);
+		const food = currentUser.pantry.id(req.params.foodId);
+		res.render("foods/edit.ejs", {
+			title: food.title,
+			food,
+		});
+	} catch (err) {
+		console.log(err);
+		res.redirect("/");
+	}
+};
 
 const update = async (req, res) => {
-  try {
-      const currentUser = await User.findById(req.params.userId)
-      const food = currentUser.pantry.id(req.params.foodId)
+	try {
+		const currentUser = await User.findById(req.params.userId);
+		console.log("current user: ", currentUser);
+		const food = currentUser.pantry.id(req.params.foodId);
+		console.log("food: ", food);
+		food.set(req.body);
+		console.log(req.body);
+		await currentUser.save();
 
-      food.set(req.body)
-      await currentUser.save()
+		res.redirect(`/users/${currentUser._id}/foods/${req.params.foodId}`);
+	} catch (err) {
+		console.log(err);
+		res.redirect("/");
+	}
+};
 
-      res.redirect(`/users/${currentUser._id}/foods/${req.params.foodId}`)
-
-  } catch (err) {
-      console.log(err)
-      res.redirect('/')
-  }
-}
-
-
-module.exports = {User, newFood, index, foodCreate, show, deleteFood, edit, update, pantry}
+module.exports = {
+	User,
+	newFood,
+	index,
+	foodCreate,
+	show,
+	deleteFood,
+	edit,
+	update,
+	pantry,
+};
--- models/user.js
+++ models/user.js
@@ -1,40 +1,38 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const foodSchema = new mongoose.Schema({
-    title: {
-            type: String,
-            required: true,
-        },
-        description: {
-            type: String,
-            required: false,
-        },
-        recipe: {
-            type: String,
-            required: false,
-        },
-    }, 
-    
-    {timestamps: true}) 
-    
-    
-    
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    pantry: [foodSchema]
-}, {timestamps: true}) 
+const foodSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		recipe: {
+			type: String,
+			required: true,
+		},
+	},
 
+	{ timestamps: true }
+);
 
+const userSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		pantry: [foodSchema],
+	},
+	{ timestamps: true }
+);
 
-
-const User = mongoose.model('User', userSchema)
-const Food = mongoose.model('Food', foodSchema)
-
-module.exports = User, Food
+const User = mongoose.model("User", userSchema);
+module.exports = User;
